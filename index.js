a3a40c45f6dbfd8738901374ea5d603852ccfa17
LYDIA <lydiaayitsoh@gmail.com>
2025-03-19 11:41:05 +0300

    added commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob';
+ const leastFavoriteCustomer = 'some initial value'
+ var bestCustomer 
+ 
+ function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();}
+
+   function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';}
+    function changeLeastFavoriteCustomer() {
+        leastFavoriteCustomer = 'someone else';}
